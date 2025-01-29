6b2093121dc5c2c8068e2b180648d5cc5d4337b1
Abhinaba Das <tstatai02@gmail.com>
2025-01-29 12:26:30 +0530

    fourth commit

--- event-management-frontend/src/api.js
+++ event-management-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-80uu.onrender.com"; 
+const API_URL = "https://backend-wn80.onrender.com"; 
 
 const api = axios.create({
   baseURL: API_URL,
--- event-management-frontend/src/socket.js
+++ event-management-frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://backend-80uu.onrender.com"; 
+const SOCKET_URL = "https://backend-wn80.onrender.com"; 
 const socket = io(SOCKET_URL);
 
 export default socket;
