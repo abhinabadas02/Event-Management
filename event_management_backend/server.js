ad63d5132915ec7e59542c4442b323694b156126
Abhinaba Das <tstatai02@gmail.com>
2025-01-29 12:12:53 +0530

    third commit

--- event_management_backend/server.js
+++ event_management_backend/server.js
@@ -15,7 +15,12 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    methods:["POST","GET","PUT","DELETE"],
+    credentials: true
+  }
+));
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
